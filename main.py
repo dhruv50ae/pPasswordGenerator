f04aac1c29927f53a2f6cbc070ce1003a565bfef
dhruv50ae <linkdp2012@gmail.com>
2022-04-06 20:41:37 +0100

    Created an average height calculator

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-fruits = ["Apple", "Banana", "Cabbage"]
-for fruit in fruits:
-    print(fruit)
+studentHeights = input(
+    "Input a list of student heights ").split()
+for n in range(0, len(studentHeights)):
+    studentHeights[n] = int(studentHeights[n])
+
+totalHeight = 0
+
+for height in studentHeights:
+    totalHeight += height
+numberOfStudent = 0
+for student in studentHeights:
+    numberOfStudent += 1
+averageHeight = round(totalHeight/numberOfStudent)
+print(averageHeight)

dc937e8c7b9ab02a28aa5f6210f4c038f0839b87
dhruv50ae <linkdp2012@gmail.com>
2022-04-07 14:47:27 +0100

    Created a Sum calc

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-studentScores = input(
-    "Input a list of student scores with a space between them. ").split()
-for n in range(0, len(studentScores)):
-    studentScores[n] = int(studentScores[n])
-print(studentScores)
-highestScore = 0
-for score in studentScores:
-    if score > highestScore:
-        highestScore = score
-print(f"The highest score in the class is: {highestScore}")
+total = 0
+
+for number in range(1, 101):
+    total += number
+print(total)

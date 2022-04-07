36458a4a163f45622e02cfa087c67bbe155fd9be
dhruv50ae <linkdp2012@gmail.com>
2022-04-07 12:30:14 +0100

    Created a highest score finder

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-studentHeights = input(
-    "Input a list of student heights ").split()
-for n in range(0, len(studentHeights)):
-    studentHeights[n] = int(studentHeights[n])
-
-totalHeight = 0
-
-for height in studentHeights:
-    totalHeight += height
-numberOfStudent = 0
-for student in studentHeights:
-    numberOfStudent += 1
-averageHeight = round(totalHeight/numberOfStudent)
-print(averageHeight)
+studentScores = input(
+    "Input a list of student scores with a space between them. ").split()
+for n in range(0, len(studentScores)):
+    studentScores[n] = int(studentScores[n])
+print(studentScores)
+highestScore = 0
+for score in studentScores:
+    if score > highestScore:
+        highestScore = score
+print(f"The highest score in the class is: {highestScore}")
